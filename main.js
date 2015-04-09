7fb4b1cb52ce66bb3ac24b036d077da8123bd104
Viktor Kronvall <considerate@n160-p28.kthopen.kth.se>
2015-04-09 14:00:51 +0200

    Basic Mock API

---
+++ main.js
@@ -0,0 +1,31 @@
+var koa = require('koa');
+var app = koa();
+var router = require('koa-router')();
+var fs = require('fs');
+var thunkify = require('thunkify');
+var readFile = thunkify(fs.readFile);
+var jws = require('jws');
+
+router.get('/users', function* () {
+   var userJson = yield readFile('users.json');
+   var users = JSON.parse(userJson);
+   this.body = {
+     users: users
+   };
+});
+
+router.post('/users/:userid/login', function* () {
+    var userid = this.params.userid;
+    var token = jws.sign({
+       header: {alg: 'HS256'},
+       payload: {id: userid},
+       secret: 'This is a very secret secret, do not tell anyone.'
+    });
+    this.body = {
+      token: token
+    };
+});
+
+
+app.use(router.routes());
+app.listen(8888);
