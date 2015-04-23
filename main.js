32fe97476ea7ff147e6d7956c1520ceac6e26f7b
Viktor Kronvall <viktor.kronvall@gmail.com>
2015-04-23 23:44:10 +0200

    Add logging and fix JWT header.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var thunkify = require('thunkify');
 var readFile = thunkify(fs.readFile);
 var jws = require('jws');
+var logger = require('koa-logger');
 
 router.get('/users', function* () {
    var userJson = yield readFile('users.json');
@@ -19,16 +20,23 @@ router.get('/users/:userid', function*() {
     var userJson = yield readFile('users.json');
     var users = JSON.parse(userJson);
     var me = users.filter(function(user) {
-       return user.id = userid;
+       return (user.id == userid);
     })[0];
-    this.body = me || {};
+    if(!me) {
+        this.throw(404);
+    } else {
+        this.body = me;
+    }
 })
 
 router.post('/users/:userid/login', function* () {
     var userid = this.params.userid;
+    var expiery = new Date();
+    expiery.setDate(expiery.getDate()+3);
     var token = jws.sign({
-       header: {alg: 'HS256'},
+       header: {alg: 'HS256', typ: 'JWT'},
        payload: {id: userid},
+       exp: expiery,
        secret: 'This is a very secret secret, do not tell anyone.'
     });
     this.body = {
@@ -37,5 +45,5 @@ router.post('/users/:userid/login', function* () {
 });
 
 
-app.use(router.routes());
+app.use(logger()).use(router.routes());
 app.listen(8888);
